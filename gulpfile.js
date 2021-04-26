eadd2a8a63b1be0d94a79cc2983570ada66a2236
AlDekker <alexander-dekker@yandex.ru>
2021-04-26 17:02:21 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const sourceFolder = 'src';
+const resultFolder = 'dist';
+
+const { series, parallel, src, dest, watch } = require('gulp');
+const imageResize   = require('gulp-image-resize');
+const imagemin      = require('gulp-imagemin');
+const del           = require('del');
+
+/* Optimization */
+function optimization() {
+  return src(`${sourceFolder}/**/*.{jpg,png}`)
+  .pipe(imageResize({
+    width : 800,
+    height : 0,
+    crop : false,
+    upscale : false
+  }))
+  .pipe(imagemin())
+  .pipe(dest(resultFolder))
+}
+
+/* Clearing */
+function clearResultFolder() {
+  return del(resultFolder, { force: true })
+}
+
+exports.run   = optimization;
+exports.clear = clearResultFolder;
